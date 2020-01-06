df3c84dc81e28809173d6a31fcfc3b53a5136dbf
Brunohes <bruno>
2020-01-06 14:32:58 -0300

    New Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,88 +1,23 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			tsx: true
+		}
+	},
+	plugins: ['@typescript-eslint'],
 	extends: [
 		'eslint:recommended',
-		'prettier',
-		'prettier/@typescript-eslint',
+		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:react/recommended',
+		'plugin:react/recommended'
 	],
-	plugins: ['@typescript-eslint', 'react'],
-	env: {
-		browser: true,
-		commonjs: true,
-		node: true,
-		mongo: true,
-		es6: true,
-	},
-	parserOptions: {
-		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-		sourceType: 'module', // Allows for the use of imports
-		ecmaFeatures: {
-			jsx: true, // Allows for the parsing of JSX
-		},
-		project: './tsconfig.json',
-	},
-	settings: {
-		react: {
-			createClass: 'createReactClass', // Regex for Component Factory to use,
-			// default to "createReactClass"
-			pragma: 'React', // Pragma to use, default to "React"
-			version: 'detect', // React version. "detect" automatically picks the version you have installed.
-			// You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-			// default to latest and warns if missing
-			// It will default to "detect" in the future
-			flowVersion: '0.53', // Flow version
-		},
-		propWrapperFunctions: [
-			// The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-			'forbidExtraProps',
-			{ property: 'freeze', object: 'Object' },
-			{ property: 'myFavoriteWrapper' },
-		],
-		linkComponents: [
-			// Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-			'Hyperlink',
-			{ name: 'Link', linkAttribute: 'to' },
-		],
-	},
 	rules: {
 		// REACT
 		'react/prop-types': ['off'],
 
-		// TSLint
-		'@typescript-eslint/array-type': ['error', 'generic'],
-		'@typescript-eslint/class-name-casing': 'error',
-		'@typescript-eslint/indent': ['off'],
-		'@typescript-eslint/interface-name-prefix': ['error', 'always'],
-		'@typescript-eslint/member-delimiter-style': 'off',
-		'@typescript-eslint/no-use-before-define': 'warn',
-		'@typescript-eslint/no-empty-interface': [
-			'error',
-			{
-				allowSingleExtends: true,
-			},
-		],
-		'@typescript-eslint/no-explicit-any': 'warn',
-		'@typescript-eslint/no-inferrable-types': [
-			'error',
-			{
-				ignoreParameters: true,
-				ignoreProperties: true,
-			},
-		],
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/no-unused-vars': 'off',
-		'@typescript-eslint/explicit-function-return-type': [
-			'warn',
-			{
-				allowExpressions: true,
-				allowTypedFunctionExpressions: false,
-			},
-		],
-
-		// ESLint
+		// JAVASCRIPT
 		'no-console': ['warn'],
 		indent: ['error', 'tab'],
 		'max-len': [
@@ -91,73 +26,38 @@ module.exports = {
 				code: 80,
 				tabWidth: 4,
 				ignoreUrls: true,
-				ignoreStrings: true,
-			},
+				ignoreStrings: true
+			}
 		],
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
 		'max-depth': ['error', 3],
-		complexity: ['warn', 20],
+		// complexity: ['warn', 20],
 		curly: ['error', 'multi-line', 'consistent'],
-		'no-async-promise-executor': 'warn',
-		'no-await-in-loop': 'warn',
-		'no-template-curly-in-string': 'error',
-		'no-eval': 'error',
-		'no-script-url': 'error',
-		'no-self-compare': 'error',
-		'no-useless-concat': 'error',
+		'no-await-in-loop': 'error',
 		yoda: 'error',
-		'no-label-var': 'error',
-		'no-extra-bind': 'error',
-		'no-extra-label': 'error',
-		'no-implicit-coercion': 'error',
-		'no-implied-eval': 'error',
-		'array-callback-return': 'warn',
-		'block-scoped-var': 'error',
-		'no-cond-assign': ['error', 'always'],
-		'default-case': 'error',
-		'dot-location': ['error', 'property'],
-		'dot-notation': 'warn',
-		eqeqeq: 'error',
-		'no-alert': 'error',
-		'no-else-return': ['error', { allowElseIf: false }],
-		'no-lonely-if': 'error',
-		'no-empty-function': 'error',
-		'no-new': 'error',
-		'no-new-wrappers': 'error',
-		'no-return-assign': ['error', 'always'],
-		'no-new-func': 'error',
-		'no-eq-null': 'error',
-		'no-multi-spaces': 'error',
-		'require-await': 'warn',
-		'vars-on-top': 'error',
-		'jsx-quotes': ['error', 'prefer-double'],
 		'no-var': 'error',
-		'prefer-const': ['error', { destructuring: 'all' }],
-		'no-useless-rename': 'error',
-		'template-curly-spacing': ['warn', 'always'],
-		'array-bracket-newline': ['warn', 'consistent'],
-		'array-bracket-spacing': [
-			'warn',
-			'always',
-			{
-				singleValue: true,
-				objectsInArrays: false,
-				arraysInArrays: false,
-			},
-		],
-		'block-spacing': 'warn',
-		'brace-style': ['error', '1tbs'],
 		camelcase: ['error'],
-		'comma-dangle': ['warn', 'always'],
-		'computed-property-spacing': ['warn', 'always'],
-		'func-call-spacing': ['warn', 'never'],
-		'line-comment-position': ['warn', { position: 'above' }],
-		'lines-around-comment': [
+		'comma-dangle': [
 			'warn',
-			{ beforeBlockComment: true, beforeLineComment: true },
+			{
+				arrays: 'never',
+				objects: 'never',
+				imports: 'never',
+				exports: 'never',
+				functions: 'never'
+			}
 		],
-		'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
-	},
+
+		// TYPESCRIPT
+		'@typescript-eslint/member-delimiter-style': 'off',
+		'@typescript-eslint/interface-name-prefix': ['error', 'always'],
+		'@typescript-eslint/explicit-function-return-type': [
+			'warn',
+			{
+				allowExpressions: true
+			}
+		]
+	}
 }
